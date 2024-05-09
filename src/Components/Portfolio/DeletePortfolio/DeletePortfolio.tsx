865e0c1d04b8c99635dd2f77b434481439ea1b97
MehmetEmin94 <emin.aykan.ea@gmail.com>
2024-05-09 15:30:37 +0300

    portfolio

--- src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -1,14 +1,15 @@
 import React, { SyntheticEvent } from 'react'
+import { PortfolioGet } from '../../../Models/Portfolio';
 
 interface Props {
     onPortfolioDelete:(e:SyntheticEvent)=>void;
-    portfolioValue:string;
+    portfolioValue:PortfolioGet;
 }
 
 const DeletePortfolio = ({onPortfolioDelete,portfolioValue}: Props) => {
   return (
     <form onSubmit={onPortfolioDelete}>
-        <input hidden={true} value={portfolioValue}/>
+        <input hidden={true} value={portfolioValue.symbol}/>
         <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
           X
         </button>
--- src/Pages/SearchPage/SearchPage.tsx
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -1,16 +1,19 @@
-import { ChangeEvent, SyntheticEvent, useState } from 'react'
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from 'react'
 import Search from '../../Components/Search/Search'
 import ListPortfolio from '../../Components/Portfolio/ListPortfolio/ListPortfolio'
 import CardList from '../../Components/CardList/CardList'
 import { CompanySearch } from '../../company'
 import { searchCompanies } from '../../api'
+import { PortfolioGet } from '../../Models/Portfolio'
+import { portfolioAddAPI, portfolioDeleteAPI, portfolioGetAPI } from '../../Services/PortfolioService'
+import { toast } from 'react-toastify'
 
 interface Props {}
 
 const SearchPage = (props: Props) => {
     const [search,setSearch]=useState<string>("");
 
-  const [portfolioValues,setPortfolioValues]=useState<string[]>([]);
+  const [portfolioValues,setPortfolioValues]=useState<PortfolioGet[] | null>([]);
 
   const [searchResult, setSearchResult]=useState<CompanySearch[]>([]);
 
@@ -21,20 +24,45 @@ const SearchPage = (props: Props) => {
         console.log(e);
     }
 
+    useEffect(() => {
+      getPortfolio();
+    },[])
+
+    const getPortfolio = () => {
+      portfolioGetAPI()
+      .then ((res) => {
+        if (res?.data) {
+          setPortfolioValues(res?.data);
+        }
+      }).catch(() => {
+        toast.warning("Could not portfolio values");
+      });
+    }
+
     const onPortfolioCreate=(e:any)=>{
       e.preventDefault();
-      const exists=portfolioValues.find((value)=>value===e.target[0].value);
-      if(exists) return;
-      const updatedPortfolio=[...portfolioValues,e.target[0].value];
-      setPortfolioValues(updatedPortfolio);
+      portfolioAddAPI(e.target[0].value)
+      .then((res)=> {
+        if (res?.status===204) {
+          toast.success("Stock added to portfolio");
+          getPortfolio();
+        }
+      }).catch(() => {
+        toast.warning("Could not create portfolio item!");
+      })
     }
 
     const onPortfolioDelete=(e:any)=>{
       e.preventDefault();
-      const removed=portfolioValues.filter((value)=>{
-        return value!==e.target[0].value;
-      });
-      setPortfolioValues(removed);
+      portfolioDeleteAPI(e.target[0].value)
+      .then((res)=> {
+        if (res?.status===200) {
+          toast.success("Stock deleted from portfolio");
+          getPortfolio();
+        }
+      }).catch(() => {
+        toast.warning("Could not delete portfolio item!");
+      })
     }
 
     const onSearchSubmit=async (e:SyntheticEvent)=>{
@@ -54,7 +82,7 @@ const SearchPage = (props: Props) => {
   return (
     <div className="App">
       <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete}/>
+      <ListPortfolio portfolioValues={portfolioValues!} onPortfolioDelete={onPortfolioDelete}/>
       <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
       {serverError && <div>Unable to connect to API</div>}
     </div>
